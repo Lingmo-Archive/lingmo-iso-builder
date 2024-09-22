205195e02236b6ae35eedaf6aba062b6697f9680
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-09-22 20:53:36 +0800

    Add iso builder

--- build.sh
+++ build.sh
@@ -14,6 +14,55 @@ get_apk_download_link() {
         jq -r ".assets.links[] | select(.name | contains(\"$(arch)\")) | .url"
 }
 
+make_iso_image() {
+    grub-mkimage -o "${workdir}/core.img" -O i386-pc -C xz --prefix=/boot/grub biosdisk iso9660 normal
+    cat /usr/lib/grub/i386-pc/cdboot.img "${workdir}/core.img" >"${isoroot}/boot/grub/grub_eltorito"
+
+    grub-mkstandalone -O i386-efi \
+        --compress="xz" \
+        --locales="" \
+        --themes="" \
+        --fonts="" \
+        --output="${isoroot}/EFI/BOOT/BOOTIA32.EFI" \
+        "boot/grub/grub.cfg=$(dirname "$0")/grub-embed.cfg"
+
+    grub-mkstandalone -O x86_64-efi \
+        --compress="xz" \
+        --locales="" \
+        --themes="" \
+        --fonts="" \
+        --output="${isoroot}/EFI/BOOT/BOOTx64.EFI" \
+        "boot/grub/grub.cfg=$(dirname "$0")/grub-embed.cfg"
+
+    mkfs.fat -C -n EFIBOOT efiboot.img 8192
+    mmd -i efiboot.img ::/EFI ::/EFI/BOOT
+    mcopy -vi efiboot.img \
+        "${isoroot}/EFI/BOOT/BOOTIA32.EFI" \
+        "${isoroot}/EFI/BOOT/BOOTx64.EFI" \  ::/EFI/BOOT/
+
+    xorriso \
+        -as mkisofs \
+        -iso-level 3 \
+        -full-iso9660-filenames \
+        --mbr-force-bootable -partition_offset 16 \
+        -joliet -joliet-long -rational-rock \
+        --grub2-boot-info \
+        --grub2-mbr /usr/lib/grub/i386-pc/boot_hybrid.img \
+        -eltorito-boot \
+        boot/grub/grub_eltorito \
+        -no-emul-boot \
+        -boot-load-size 4 \
+        -boot-info-table \
+        --eltorito-catalog boot/grub/boot.cat \
+        -eltorito-alt-boot \
+        -e --interval:appended_partition_2:all:: \
+        -no-emul-boot \
+        -isohybrid-gpt-basdat \
+        -append_partition 2 C12A7328-F81F-11D2-BA4B-00A0C93EC93B efiboot.img \
+        -output "${isofile}" \
+        "${isoroot}"
+}
+
 if [ $UID -ne 0 ]; then
     echo "Must be root"
     exit 1
@@ -35,10 +84,18 @@ cp "$(dirname "$0")/chroot.sh" "${workdir}/rootfs"
 log "Entering chroot"
 arch-chroot "${workdir}/rootfs" /chroot.sh
 
-rootfs_size=$(du -sh "${workdir}/rootfs" | awk '{print $1}')
-log "RootFS size: ${rootfs_size}"
+du -sh "${workdir}/rootfs"
 
 log "Making SquashFS image"
 mksquashfs "${workdir}/rootfs" "${workdir}/rootfs.squashfs" -comp xz -Xbcj x86 -b 1M -Xdict-size 1M
-rootfs_size=$(du -h "${workdir}/rootfs.squashfs" | awk '{print $1}')
-log "SquashFS image size: ${rootfs_size}"
+du -h "${workdir}/rootfs.squashfs"
+
+log "Making ISO image"
+isofile="$(dirname "$0")/lingmo-installer-$(uname -m)-$(date +%Y.%m.%d).iso"
+isoroot="${workdir}/isoroot"
+mkdir -pv "${isoroot}/boot/grub"
+cp -v "$(dirname "$0")/grub.cfg" "${isoroot}/boot/grub"
+make_iso_image
+du -h "$(dirname "$0")/*.iso"
+
+log "Done!"
